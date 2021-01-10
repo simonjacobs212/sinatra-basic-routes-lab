6a9def8be9e4995bf121b464dce8b1d97b3c47cd
Simon Jacobs <simonadlerjacobs@gmail.com>
2021-01-10 15:18:02 -0500

    commit tree 112f0c3021c62a04a8324deb3ad4bcf768b83bfc

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Simon"
+    end
+
+    get '/hometown' do
+        "My hometown is NYC"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Who built the moon...at least right now."
+    end
+
 end
